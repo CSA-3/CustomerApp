f02de4a2dfcd3ad839aeec0faf11ba0dbee99cbf
CSA-3 <84491242+CSA-3@users.noreply.github.com>
2021-07-03 18:21:44 +0530

    Update ForgotPassword.js

--- components/ForgotPassword.js
+++ components/ForgotPassword.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import firebase from 'firebase';
+// import firebase from 'firebase';
 import {StyleSheet, TouchableOpacity, View,Text,Image, TextInput,ScrollView, Alert} from 'react-native'
 
 
@@ -16,13 +16,14 @@ function ForgotPassword({navigation}) {
   
     return(
         <View style={styles.container}>
+            <Image source={require('C:/Users/Singh.DESKTOP-VM4G1KR/Desktop/Neha/Project/cakeapp/images/sn.png')} style={styles.backgroundImage} />
             <View style={styles.header}>
-                <Image source={require('C:/Users/Singh.DESKTOP-VM4G1KR/Desktop/Neha/Project/cakeapp/images/cake.jpg')} style={styles.logo}></Image>
+                <Image source={require('C:/Users/Singh.DESKTOP-VM4G1KR/Desktop/Neha/Project/cakeapp/images/cake.png')} style={styles.logo}></Image>
                
             </View> 
             <ScrollView>
                 <View style={styles.login}>
-                    <Text style={{textAlign:'center',fontSize:18,marginBottom: 20}}>Forgot Password?</Text>
+                    <Text style={{textAlign:'center',fontSize:20,marginBottom: 20,marginTop: 90,fontWeight:'bold'}}>Forgot Password?</Text>
                     <Text style={styles.error}>{(disable.Email) && 'Invalid Email id'}</Text>
                     <TextInput style={styles.field} 
                         placeholder={'  Enter your Email ID'} placeholderTextColor="#202020"
@@ -72,34 +73,39 @@ const styles = StyleSheet.create({
         flex:1,
         alignItems:"center",
         justifyContent: 'center',
-        backgroundColor:'white',
+        //backgroundColor:'#8EFDF6',
     },
-    header: {  
-        height:250,
+    header: {    
+        height:140, 
+        width:345,
         alignItems:"center",
-        justifyContent:"center", 
-        paddingTop:20,
-        backgroundColor:"white", 
+        justifyContent:"center",  
+        borderBottomRightRadius :70, 
+        borderTopLeftRadius :70, 
+        backgroundColor:'#F9C0F8',
+        marginTop:8,
+        borderColor:"#FF6FFC" ,
+        borderWidth:2,    
     },    
     logo:{
-        height:70,
-        width:550,
-        borderRadius:30, 
+        height:65,
+        width:311,
+        borderRadius:70, 
+        //borderColor:"#FF6FFC" ,
         resizeMode: 'contain',
+        //borderWidth:2,    
     },
     field: {
-        borderWidth:2,
-        flexDirection:"row" ,
-        marginLeft:4,
-        marginRight:10,
-        marginTop:20,
+        borderWidth:2, 
+        marginLeft:4,  
+        backgroundColor: '#BCFFFB',  
         borderColor:"black" ,
-        height:40,
-        fontSize:15,
+        height:40, 
+        fontSize:17,
         padding:10,
         paddingVertical:5,
         borderRadius: 25, 
-        width: "95%", 
+        width: 330, 
     },
     error: {
         fontSize:15,
@@ -109,16 +115,27 @@ const styles = StyleSheet.create({
     },
     button: {
         marginTop:15,
-        marginLeft:45,
-        backgroundColor: 'pink', 
-        width: "60%", 
+        alignSelf:"center",
+        backgroundColor: '#8DFEF7', 
+        width: "50%", 
         borderRadius:30, 
         fontSize:20,  
         borderColor:"black" ,
         color:"black",
         borderWidth:2,    
         height:32,
-        textAlign:"center", 
-    }
+        textAlign:"center",
+    },
+    backgroundImage: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        opacity: 0.7,
+        height:'100%', 
+        width:'100%'
+      }
+
 });  
  
