280f5aefcca87094ae8fa431185f39a8f66154cc
CSA-3 <84491242+CSA-3@users.noreply.github.com>
2021-07-03 18:22:33 +0530

    Update login.js

--- components/login.js
+++ components/login.js
@@ -1,9 +1,17 @@
 import React, {useState} from 'react'
 import firebase from 'firebase';
 import {StyleSheet, TouchableOpacity, View,Text,Image, TextInput,ScrollView, Alert} from 'react-native'
-
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 function Login({navigation}) {
+  const [icon, setIcon] = useState("eye-slash")
+    const [hidePassword, setHidePassword] = useState(true)      
+    const changeIcon = () => {
+        icon !== "eye-slash"
+        ? (setIcon("eye-slash"), setHidePassword(true))
+        : (setIcon("eye"), setHidePassword(false))        
+    }
+
   function getCredentials() {
         const dbRef = firebase.database().ref();
         const dbtable=dbRef.child("customers"); 
@@ -47,31 +55,55 @@ function Login({navigation}) {
         password:''
     })
      
+    const [passwordVisibility, setPasswordVisibility] = useState(true);
+    const [rightIcon, setRightIcon] = useState('eye');
+ 
+
+  function handlePasswordVisibility() {
+    if (rightIcon === 'eye') {
+      setRightIcon('eye-off');
+      setPasswordVisibility(!passwordVisibility);
+    } else if (rightIcon === 'eye-off') {
+      setRightIcon('eye');
+      setPasswordVisibility(!passwordVisibility);
+    }
+  }
     const [dbObj,setdbObj] = useState([(getCredentials())])
     return(
         <View style={styles.container}>
+            <Image source={require('C:/Users/Singh.DESKTOP-VM4G1KR/Desktop/Neha/Project/cakeapp/images/fl.png')} style={styles.backgroundImage} />
             <View style={styles.header}>
-                <Image source={require('C:/Users/Singh.DESKTOP-VM4G1KR/Desktop/Neha/Project/cakeapp/images/cake.jpg')} style={styles.logo}></Image>
+                <Image source={require('C:/Users/Singh.DESKTOP-VM4G1KR/Desktop/Neha/Project/cakeapp/images/cake.png')} style={styles.logo}></Image>
                
             </View> 
             <ScrollView>
-                <View style={styles.login}>
-                    <Text style={{textAlign:'center',fontSize:18,marginBottom: 20}}>Please Login</Text>
-                    <TextInput style={styles.field} placeholderTextColor="#202020" placeholder="Email"  
+                <View >
+                    <Text style={{textAlign:'center',fontSize:18,marginBottom: 20,marginTop: 80}}>Please Login</Text>
+                    <TextInput style={styles.field} placeholderTextColor="black" placeholder="  Email"  
                         onChangeText={(text)=>{ 
                             setUserinput({
                             ...userinput,
                             username:text});
                         }}
                     /> 
-                    <TextInput style={[styles.field,{marginTop:20}]} placeholderTextColor="#202020" placeholder="Password" rightIcon={rightIcon}  handlePasswordVisibility={handlePasswordVisibility}
-                        onChangeText={(text)=>{ 
-                            setUserinput({
-                            ...userinput,
-                            password:text});
-                        }}
+                    
+                    <View style={[styles.field,{flexDirection:'row',marginLeft:0,marginRight:0,marginTop:30,justifyContent:'space-evenly'}]}>
+                    <TextInput
+                        placeholder={'Password'}
+                        secureTextEntry={hidePassword}
+                        placeholderTextColor="black"
+                        style={{width:300,marginLeft:10,marginRight:10,fontSize:15}}
+                        onChangeText={(text)=>{
+                            setInput({
+                                ...input,
+                                Password:text
+                            });
+                        }
+                        }
                     />
-                    <TouchableOpacity><Text style={{textDecorationLine:"underline",marginTop:20,marginLeft:65,}} onPress={(e)=>navigation.navigate("ForgotPassword")} >Forgot Password?</Text></TouchableOpacity>
+                    <Icon style={{width:40}} name={icon} size={20} onPress={() => changeIcon()}/>
+                    </View>
+                    <TouchableOpacity><Text style={{textDecorationLine:"underline",marginTop:20, fontWeight:'bold',textAlign:'center'}} onPress={(e)=>navigation.navigate("ForgotPassword")} >Forgot Password?</Text></TouchableOpacity>
                     <TouchableOpacity 
                      onPress={(e)=>{
                         
@@ -118,9 +150,9 @@ function Login({navigation}) {
                         } 
                     }}><Text style={styles.button}>Login</Text></TouchableOpacity>
                     <Text>{'\n'}</Text>
-                    <View style={{flexDirection:"row"}}>
-                        <Text>Don't have an account? </Text>
-                        <TouchableOpacity onPress={(e)=>navigation.navigate("Registration")}><Text style={{textDecorationLine:"underline",color:"red"}}>Register Here</Text></TouchableOpacity>
+                    <View style={{flexDirection:"row",fontWeight:'bold',alignSelf:"center"}}>
+                        <Text style ={{ fontWeight:'bold'}}>Don't have an account? </Text>
+                        <TouchableOpacity onPress={(e)=>navigation.navigate("Registration")}><Text style={{textDecorationLine:"underline",color:"red", fontWeight:'bold'}}>Register Here</Text></TouchableOpacity>
                     </View>
             </View>
             </ScrollView>
@@ -137,34 +169,40 @@ const styles = StyleSheet.create({
         flex:1,
         alignItems:"center",
         justifyContent: 'center',
-        backgroundColor:'white',
+        //backgroundColor:'#8EFDF6',
     },
-    header: {  
-        height:250,
+    header: {    
+        height:140, 
+        width:345,
         alignItems:"center",
-        justifyContent:"center", 
-        paddingTop:20,
-        backgroundColor:"white", 
+        justifyContent:"center",  
+        borderBottomRightRadius :70, 
+        borderTopLeftRadius :70, 
+        backgroundColor:'#F9C0F8',
+        marginTop:8,
+        borderColor:"#FF6FFC" ,
+        borderWidth:2,    
     },    
     logo:{
-        height:70,
-        width:550,
-        borderRadius:30, 
-        resizeMode: 'contain',
-    },
+      height:65,
+      width:311,
+      borderRadius:70, 
+      //borderColor:"#FF6FFC" ,
+      resizeMode: 'contain',
+      //borderWidth:2,    
+  },
     field: {
-        borderWidth:2,
-        flexDirection:"row" ,
-        marginLeft:4,
-        marginRight:10,
-        marginTop:20,
+        borderWidth:2, 
+        marginLeft:4, 
+        //  backgroundColor:"#F0BEFA", 
+        backgroundColor: '#8DFEF7',  
         borderColor:"black" ,
-        height:40,
-        fontSize:15,
+        height:40, 
+        fontSize:17,
         padding:10,
         paddingVertical:5,
         borderRadius: 25, 
-        width: "95%", 
+        width: 330, 
     },
     error: {
         fontSize:15,
@@ -174,9 +212,9 @@ const styles = StyleSheet.create({
     },
     button: {
         marginTop:15,
-        marginLeft:50,
-        backgroundColor: 'pink', 
-        width: "60%", 
+        alignSelf:"center",
+        backgroundColor: '#8DFEF7', 
+        width: "50%", 
         borderRadius:30, 
         fontSize:20,  
         borderColor:"black" ,
@@ -184,6 +222,19 @@ const styles = StyleSheet.create({
         borderWidth:2,    
         height:32,
         textAlign:"center",
-    }
+    },
+    backgroundImage: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        opacity: 0.5,
+        height:'100%', 
+        width:'100%'
+      }
+
+
+
 });  
  
