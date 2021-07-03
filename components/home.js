a3c638a6acdbb951139842307638a005ae7d725a
CSA-3 <84491242+CSA-3@users.noreply.github.com>
2021-07-03 18:22:12 +0530

    Update home.js

--- components/home.js
+++ components/home.js
@@ -1,47 +1,84 @@
 import React from 'react';
-import {StyleSheet, View,Text,Button, TouchableOpacity} from 'react-native';
+import {StyleSheet, View,Text,Image, TouchableOpacity} from 'react-native';
 
 
 
 export default function Home({navigation}) {
-
-    const pressHandler =() => {
-        navigation.navigate("Registration"); 
-    }
-    const pressHandler2 =() => {
-        navigation.navigate("Login"); 
-    }
+ 
   
     return(
-        <View>   
-            <Text style={{fontSize:40,fontWeight:'bold',color:'#0B0DE0',marginLeft:70 }} onPress={pressHandler}>WELCOME TO</Text> 
-            <Text style={{fontSize:40,fontWeight:'bold',color:'orange',marginLeft:110 }} onPress={pressHandler}> CAKE </Text> 
-            <Text style={{fontSize:40,fontWeight:'bold',color:'#F613F3',marginLeft:120 }} onPress={pressHandler}> POP </Text> 
-            <Text style={{fontSize:40,fontWeight:'bold',color:'lightgreen',marginLeft:110 }} onPress={pressHandler}> RUSH </Text> 
-            <TouchableOpacity style={styles.button}> 
-                   <Text style={{fontSize:20,fontWeight:'bold',color:'white'}} onPress={pressHandler}> Sign Up </Text> 
+        <View style={styles.container}>  
+            <Image source={require('C:/Users/Singh.DESKTOP-VM4G1KR/Desktop/Neha/Project/cakeapp/images/co.png')} style={styles.backgroundImage} />
+            <View style={styles.header}>
+                <Image source={require('C:/Users/Singh.DESKTOP-VM4G1KR/Desktop/Neha/Project/cakeapp/images/cake.png')} style={styles.logo}></Image>
+               
+            </View> 
+                
+            <TouchableOpacity > 
+                   <Text  style={styles.button}  onPress={(e)=>navigation.navigate("Registration")}> Sign Up </Text> 
             </TouchableOpacity>
-            <TouchableOpacity style={styles.button} > 
-                   <Text style={{fontSize:20,fontWeight:'bold',color:'white'}} onPress={pressHandler2}> Login  </Text> 
+            <TouchableOpacity  > 
+                   <Text style={styles.button}   onPress={(e)=>navigation.navigate("Login")}> Login </Text> 
+            </TouchableOpacity> 
+            <TouchableOpacity  > 
+                   <Text style={styles.button}   onPress={(e)=>navigation.navigate("Blog")}> Read Blogs </Text> 
+            </TouchableOpacity> 
+            <TouchableOpacity  > 
+                   <Text style={styles.button}   onPress={(e)=>navigation.navigate("Cart")}> Go to Cart </Text> 
             </TouchableOpacity> 
         </View>
     )
  
 }
 const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:"center",
+        justifyContent: 'center',
+        //backgroundColor:'#8EFDF6',
+    },
+    header: {    
+        height:140, 
+        width:345,
+        alignItems:"center",
+        justifyContent:"center",  
+        borderBottomRightRadius :70, 
+        borderTopLeftRadius :70, 
+        backgroundColor:'#F9C0F8',
+        marginTop:8,
+        borderColor:"#FF6FFC" ,
+        borderWidth:2,    
+    },    
+    logo:{
+        height:65,
+        width:311,
+        borderRadius:70, 
+        //borderColor:"#FF6FFC" ,
+        resizeMode: 'contain',
+        //borderWidth:2,    
+    },
     button: {
-        backgroundColor: 'pink',
-        alignItems:'center',
-        width: 150,
-        borderRadius:15,
-        padding:5 ,
-        marginBottom:20,
-        marginTop:20,
-        marginLeft:100,
-        borderColor:"black" ,
-        color:"white",
-        borderWidth:2 
-    }
-    
+        marginTop:15,
+        alignSelf:"center",
+        backgroundColor: '#F9C0F8', 
+        width: 130, 
+        borderRadius:30, 
+        fontSize:20,  
+        borderColor:"#FF6FFC" ,
+        color:"black",
+        borderWidth:2,    
+        height:32,
+        textAlign:"center",
+    },
+    backgroundImage: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        opacity: 0.6,
+        height:'100%', 
+        width:'100%'
+      }
 
 });
